2fba4dff6a6918aa15b20679bf49ace6da468427
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-11-15 15:22:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,39 @@ import { elements } from "chart.js";
 
 function App() {
   const layout = [
-    { i: "a", x: 0, y: 0, w: 6, h: 7, static : true, element: <BarChart />},
+    { i: "a", x: 0, y: 0, w: 6, h: 7, static: true, element: <BarChart /> },
     { i: "b", x: 6, y: 0, w: 6, h: 7, element: <AreaChart /> },
-    { i: "c", x: 8, y: 0, w: 6, h: 10, minW: 4, maxW: 8, minH: 8, maxH: 12, element: <Piechart /> },
+    {
+      i: "c",
+      x: 8,
+      y: 0,
+      w: 6,
+      h: 10,
+      minW: 4,
+      maxW: 8,
+      minH: 8,
+      maxH: 12,
+      element: <Piechart />,
+    },
   ];
 
   return (
-    <GridLayout
-      className="layout"
-      layout={layout}
-      cols={12}
-      rowHeight={30}
-      width={1360}
-    >
-      {layout.map((item) => (
-        <div className="box" key={item.i}>{item.element}</div>
-      ))}
-    </GridLayout>
+    <>
+      <h1>Dashboard Drag&drop</h1>
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1360}
+      >
+        {layout.map((item) => (
+          <div className="box" key={item.i}>
+            {item.element}
+          </div>
+        ))}
+      </GridLayout>
+    </>
   );
 }
 
