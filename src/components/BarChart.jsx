5b44f017d11b7a5a5572d035227185f72e481d5d
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-11-15 16:19:43 -0500

    event register

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -6,10 +6,10 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import faker from 'faker';
-import React from 'react';
+} from "chart.js";
+import { Bar, getElementAtEvent } from "react-chartjs-2";
+import faker from "faker";
+import React, { useRef } from "react";
 
 ChartJS.register(
   CategoryScale,
@@ -20,39 +20,43 @@ ChartJS.register(
   Legend
 );
 const BarChart = () => {
-    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const options = {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: 'Chart.js Bar Chart',
-          },
-        },
-      };
-      const data = {
-        labels,
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          },
-          {
-            label: 'Dataset 2',
-            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-          },
-        ],
-      };
-    return (
-        <Bar options={options} data={data} />
-    );
+  const chartRef = useRef();
+  const labels = ["January", "February", "March", "April", "May", "June", "July"];
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+  const onClick = (event) => {
+    const eventRegister = getElementAtEvent(chartRef.current, event)
+    let datasetIndex = eventRegister[0].datasetIndex
+    let index = eventRegister[0].index
+    alert(`You clicked on the dataset index ${data.datasets[datasetIndex].label} and got the value corresponding to ${labels[index]}`)
+  };
+
+  return <Bar ref={chartRef} options={options} data={data} onClick={onClick} />;
 };
 
 export default BarChart;
-
-
